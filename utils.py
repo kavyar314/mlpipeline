6decb9662c20dbadc06e8ec58077c94241468aab
Kavya Ravichandran <rkavya@Kavyas-MacBook-Pro.local>
2022-05-14 17:10:24 -0500

    loading files from path

--- utils.py
+++ utils.py
@@ -1,10 +1,12 @@
 
 import os, random
 import numpy as np
+from skimage.io import imread
+from skimage import transform
 
 EXCLUDE = ['.DS_Store']
 
-def train_test_split(path, n_samples=None, split=0.8):
+def train_test_split(path, n_samples=None, split=0.8, include_classes=False):
 	'''
 	splits all the files found in the dataset into train and test splits
 
@@ -16,19 +18,38 @@ def train_test_split(path, n_samples=None, split=0.8):
 	classes = [c for c in os.listdir(path) if c not in EXCLUDE]
 	files = []
 	for c in classes:
-		files += os.listdir(os.path.join(path, c))
+		files += [(f, c) for f in os.listdir(os.path.join(path, c))]
 
 	files = [f for f in files if f not in EXCLUDE]
 
 	random.shuffle(files)
 
 	upper_limit = n_samples if n_samples is not None else len(files)
+	if not include_classes:
+		return [x[0] for x in files[:int(split*upper_limit)]], [x[0] for x in files[int(split*upper_limit)+1:upper_limit]]
+	else:
+		return files[:int(split*upper_limit)], files[int(split*upper_limit)+1:upper_limit]
 
-	return files[:int(split*upper_limit)], files[int(split*upper_limit)+1:upper_limit]
+def load_specified_files_from_path(data_path, specs, img_dim=19):
+	'''
+	data_path: the path where the files live, with one subfolder for each class, inside which all the image files live
+	specs: list of filenames to load from data_path / class / fname
+	classes: list of classes associated with the files in specs
+	'''
+	X = []
+	for fname, cname in specs:
+		x = imread(os.path.join(os.path.join(data_path, cname), fname))
+		X.append(grey_scale_resize([x], img_dim))
+	classes = [s[1] for s in specs]
+	class_numbers = dict(zip(list(set(classes)), range(len(set(classes)))))
+	return np.array(X), [class_numbers[c] for c in classes]
 
-def load_raw_data(pos_path="./data/faces_selected/", neg_path="./data/not_faces_selected/", cap_class=np.inf):
+
+
+def load_raw_data(data_path,pos_path="./data/faces_selected/", neg_path="./data/not_faces_selected/", cap_class=np.inf):
 	## TODO: make the signature less specific to the two classes etc
     paths = [pos_path, neg_path]
+    classes = os.lisd
     pos_img_path_list = os.listdir(pos_path)
     neg_img_path_list = os.listdir(neg_path)
     np.random.shuffle(pos_img_path_list)
